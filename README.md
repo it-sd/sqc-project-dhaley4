95ccd4b2d1051fa81bda9e542354a49f1c588c35
Davin Haley <dhaley4@student.cvtc.edu>
2023-03-07 14:53:28 -0600

    change to be a more simplified Javascript program

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Davin's Numbers Site
 Davin Haley, dhaley4
 
-Page for recording and displaying users and their lucky numbers, as well as displaying the number of the day.
+Page for displaying messages associated with specialized passcodes.
+Simplified from the original idea, due to the simplification of the final project requirements.
 
 Render Service: [Here](https://sqc-project-dhaley4.onrender.com)
---
+++ public/passcode.js
@@ -0,0 +1,37 @@
+function insertNumber(number) {
+  const codeDisplay = document.getElementById('codeDisplay')
+  let passcode = codeDisplay.innerText
+  if (passcode == 'Awaiting Input...') {
+    passcode = number
+  } else if (passcode.length < 6) {
+    passcode = passcode + number
+  }
+  codeDisplay.innerHTML = passcode
+  console.log(`Inserted number ${number}, making ${passcode}`)
+}
+
+function backspace() {
+  // get the codeDisplay element using Id, as well as getting the text stored in it
+  const codeDisplay = document.getElementById('codeDisplay')
+  let passcode = codeDisplay.innerText
+
+  // make sure its not "Awaiting Input...", the default for when no numbers have been entered
+  // The passcode would only be this when nothing is entered, so there is nothing to delete
+  if (passcode != 'Awaiting Input...') {
+    // if passcode length is 1, a backspace makes it zero, so change to default
+    // otherwise, make it so the passcode is what it currently is, minus the last character
+    if (passcode.length == 1) {
+      passcode = 'Awaiting Input...'
+    } else (
+      passcode = passcode.substring(0, passcode.length - 1)
+    )
+    // display the new passcode
+    codeDisplay.innerText = passcode
+  }
+}
+
+function submitPasscode() {
+  // TODO: Implement this
+  // Should check if the passcode is saved anywhere, and if it is, retrieve the associated message, and display it in the large textarea.
+  // if there is no message saved for that passcode, prompt the user to submit their own to fill the space.
+}
